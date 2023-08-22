de59628040d05ceac0dfd6df414fad6877b06958
Rahul Basu <rbasu5027@stu.d214.org>
2023-08-22 11:02:17 -0500

    Hello People kjevbwv

---
+++ TableView.swift
@@ -0,0 +1,20 @@
+//
+//  TableView.swift
+//  fgjyhkj
+//
+//  Created by Rahul A. Basu on 8/28/23.
+//
+
+import SwiftUI
+
+struct TableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TableView_Previews: PreviewProvider {
+    static var previews: some View {
+        TableView()
+    }
+}
