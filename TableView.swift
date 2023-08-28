b2fd69889c1b70700817e37dd1d7b3688d97da7a
Rahul Basu <rbasu5027@stu.d214.org>
2023-08-28 10:34:46 -0500

    Table View

--- TableView.swift
+++ TableView.swift
@@ -9,12 +9,12 @@ import SwiftUI
 
 struct TableView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello")
+        
     }
-}
-
-struct TableView_Previews: PreviewProvider {
-    static var previews: some View {
-        TableView()
+    struct TableView_Previews: PreviewProvider {
+        static var previews: some View {
+            TableView()
+        }
     }
 }
