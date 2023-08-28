a2a2ccea93f4d437c7b55704cbc66c07fa46a23d
Rahul Basu <rbasu5027@stu.d214.org>
2023-08-28 11:01:40 -0500

    Table View

--- TableView.swift
+++ TableView.swift
@@ -7,10 +7,35 @@
 
 import SwiftUI
 
+
+struct Player {
+    let position: String
+    let name: String
+}
 struct TableView: View {
     var body: some View {
-        Text("Hello")
-        
+        let players: [Player] = [
+                Player(position: "Quarterback", name: "Patrick Mahomes"),
+                Player(position: "Running Back", name: "Nich Chubb"),
+                Player(position: "Wide Receiver", name: "Darnell Mooney"),
+                Player(position: "Tight End", name: "Travis Kelce"),
+                Player(position: "Defensive End", name: "Myles Garrett"),
+                Player(position: "Linebacker", name: "Bobby Wagner"),
+                Player(position: "Cornerback", name: "Jalen Ramsey"),
+                Player(position: "Safety", name: "Tyrann Mathieu"),
+                Player(position: "Kicker", name: "Justin Tucker"),
+                Player(position: "Defensive Tackle", name: "Chris Jones")
+        ]
+               
+                List(players, id: \.name) { player in
+                                VStack(alignment: .leading) {
+                                    Text(player.position)
+                                        .font(.headline)
+                                    Text(player.name)
+                                        .font(.subheadline)
+                                        .foregroundColor(.secondary)
+                                }
+                            }
     }
     struct TableView_Previews: PreviewProvider {
         static var previews: some View {
